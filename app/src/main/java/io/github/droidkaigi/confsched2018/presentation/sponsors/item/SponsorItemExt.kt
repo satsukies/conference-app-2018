51344a5a7a066f869c6cffe5150d1b2e655f1ee1
Jumpei Matsuda <j.matsuda.bb26th@gmail.com>
2018-01-15 01:09:11 +0900

    Shrink some code and re-format

--- app/src/main/java/io/github/droidkaigi/confsched2018/presentation/sponsors/item/SponsorItemExt.kt
+++ app/src/main/java/io/github/droidkaigi/confsched2018/presentation/sponsors/item/SponsorItemExt.kt
@@ -10,41 +10,50 @@ import io.github.droidkaigi.confsched2018.R
 import io.github.droidkaigi.confsched2018.model.SponsorPlan
 
 fun getSponsorItemSpanSize(planType: SponsorPlan.Type, spanCount: Int) = when (planType) {
-    SponsorPlan.Type.Platinum, SponsorPlan.Type.TechnicalForNetwork -> spanCount / 2
-    SponsorPlan.Type.Gold, SponsorPlan.Type.Silver, SponsorPlan.Type.Supporter -> spanCount / 3
+    SponsorPlan.Type.Platinum,
+    SponsorPlan.Type.TechnicalForNetwork -> spanCount / 2
+    SponsorPlan.Type.Gold,
+    SponsorPlan.Type.Silver,
+    SponsorPlan.Type.Supporter -> spanCount / 3
 }
 
 object SponsorColor {
     @ColorInt
     @JvmStatic // to use in DataBinding
-    fun fromPlanType(context: Context, planType: SponsorPlan.Type?) = ContextCompat.getColor(context, when (planType) {
-        SponsorPlan.Type.Platinum -> R.color.sponsor_platinum
-        SponsorPlan.Type.Gold -> R.color.sponsor_gold
-        SponsorPlan.Type.Silver -> R.color.sponsor_silver
-        SponsorPlan.Type.Supporter -> R.color.sponsor_supporter
-        SponsorPlan.Type.TechnicalForNetwork -> R.color.sponsor_technical_for_network
-        else -> R.color.sponsor_supporter
-    })
+    fun fromPlanType(context: Context, planType: SponsorPlan.Type?) =
+            ContextCompat.getColor(context, when (planType) {
+                SponsorPlan.Type.Platinum -> R.color.sponsor_platinum
+                SponsorPlan.Type.Gold -> R.color.sponsor_gold
+                SponsorPlan.Type.Silver -> R.color.sponsor_silver
+                SponsorPlan.Type.Supporter -> R.color.sponsor_supporter
+                SponsorPlan.Type.TechnicalForNetwork -> R.color.sponsor_technical_for_network
+                else -> R.color.sponsor_supporter
+            })
 }
 
 interface SponsorItemExt {
     fun SponsorPlan.Type.toDimensionRatio() = when (this) {
-        SponsorPlan.Type.Platinum, SponsorPlan.Type.Supporter, SponsorPlan.Type.TechnicalForNetwork -> "H,16:9"
+        SponsorPlan.Type.Platinum,
+        SponsorPlan.Type.Supporter,
+        SponsorPlan.Type.TechnicalForNetwork -> "H,16:9"
         SponsorPlan.Type.Gold -> "H,1:1"
         SponsorPlan.Type.Silver -> "H,4:3"
     }
 
     fun ConstraintLayout.adjustAspectRatioConstraint(planType: SponsorPlan.Type, target: View) {
-        val lp = target.layoutParams as? ConstraintLayout.LayoutParams ?: throw IllegalStateException("layout structure was changed.")
+        val lp = target.layoutParams as? ConstraintLayout.LayoutParams ?: let {
+            throw IllegalStateException("layout structure was changed.")
+        }
+
         val newDimensionRatio = planType.toDimensionRatio()
 
         if (newDimensionRatio == lp.dimensionRatio) {
             return
         }
 
-        val newConstraint = ConstraintSet().apply { clone(this@adjustAspectRatioConstraint) }
-        newConstraint.setDimensionRatio(target.id, newDimensionRatio)
-        newConstraint.applyTo(this)
+        ConstraintSet().apply {
+            clone(this@adjustAspectRatioConstraint)
+            setDimensionRatio(target.id, newDimensionRatio)
+        }.applyTo(this)
     }
-
 }
